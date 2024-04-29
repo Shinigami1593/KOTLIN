47d6d8662a07ef7d8c03c0f3f2ef1105edd839f9
Ayush <rasaileeayush17@gmail.com>
2024-04-29 12:38:36 +0545

    kotlin second commit

--- app/src/main/java/com/example/first_project/GridViewActivity.kt
+++ app/src/main/java/com/example/first_project/GridViewActivity.kt
@@ -1,20 +1,22 @@
 package com.example.first_project
 
 import android.os.Bundle
+import android.widget.GridView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class GridViewActivity : AppCompatActivity() {
+    lateinit var gridView : GridView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_grid_view)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        gridView = findViewById(R.id.grid)
+
+
     }
 }
