6a22888a16f557dcd94feb8a60a6922a00908729
Ayush <rasaileeayush17@gmail.com>
2024-04-30 11:50:01 +0545

    recent commit

--- app/src/main/java/com/example/first_project/adapter/TwoWheelersAdapter.kt
+++ app/src/main/java/com/example/first_project/adapter/TwoWheelersAdapter.kt
@@ -1,25 +1,31 @@
 package com.example.first_project.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import com.example.first_project.R
 
 class TwoWheelersAdapter(var nameList: ArrayList<String>,
                          var imageList: ArrayList<Int>) : BaseAdapter() {
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return nameList.size;
     }
 
-    override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+    override fun getItem(position: Int): Any? {
+        return null;
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return 0;
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        var view : View = LayoutInflater.from(parent?.context).inflate(
+            R.layout.sample,parent,
+            false)
+
+        
     }
 
 
